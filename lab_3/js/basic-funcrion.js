07bd07df66991a8c207cb141649166f3f50e931f
ermeck <ermeck@bk.ru>
2019-05-07 19:48:22 +0400

    Добавлено задание 3 третьей лабы

---
+++ lab_3/js/basic-funcrion.js
@@ -0,0 +1,6 @@
+var msg = "Подпишитесь на нашу рассылку и получите скидку 100%";
+function updateMessage() {
+    var el = document.getElementById('message');
+    el.textContent = msg;
+}
+updateMessage();
